da09141ab6db69d61937be96357e9e715aa294de
Daniel Padrino <danswarrior1@gmail.com>
2023-07-30 23:46:14 -0300

    Organize tests with describe()

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
 //@flow
-
-import React, { Component } from 'react';
-import './App.css';
-import Grid from './listings/Grid';
-import courses from '../data/courses.json';
+import React, { Component } from "react";
+import "./App.css";
+import Grid from "./listings/Grid";
+import courses from "../data/courses.json";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {courses};
+    this.state = { courses };
   }
 
   render() {
@@ -17,12 +16,14 @@ export default class App extends Component {
         <div className="navbar-fixed">
           <nav className="blue lighten-2">
             <div className="nav-wrapper">
-              <a href="/" className="brand-logo center">something</a>
+              <a href="/" className="brand-logo center">
+                Courses
+              </a>
             </div>
           </nav>
         </div>
         <div>
-            <Grid items={this.state.courses}/>
+          <Grid items={this.state.courses} />
         </div>
       </div>
     );
--- src/components/listings/Grid.test.js
+++ src/components/listings/Grid.test.js
@@ -4,36 +4,39 @@ const numItems = data.length;
 const dataTest = data[0].title;
 const data2 = ["React Native", "React"];
 
-//numbers
-test("Number of items = 12", () => {
-  expect(numItems).toBe(12);
+describe("Numbers test", () => {
+  test("Number of items = 12", () => {
+    expect(numItems).toBe(12);
+  });
+
+  test("Number of items to be greater than or equal to 12", () => {
+    expect(numItems).toBeGreaterThanOrEqual(9);
+  });
 });
 
-test("Number of items to be greater than or equal to 12", () => {
-  expect(numItems).toBeGreaterThanOrEqual(9);
+describe("Strings test", () => {
+  test("There is a JS in this title", () => {
+    expect(dataTest).toMatch(/JS/);
+  });
+
+  test("The title contains React", () => {
+    expect(dataTest).toContain("MeteorJS");
+  });
 });
 
-//strings
-test("There is a JS in this title", () => {
-  expect(dataTest).toMatch(/JS/);
-});
-
-test("The title contains React", () => {
-  expect(dataTest).toContain("MeteorJS");
-});
-
-//arrays
-test("The list of courses contains React Native and React", () => {
+describe("Arrays and objects tests", () => {
+  test("The list of courses contains React Native and React", () => {
     expect(["React Native", "React", "MeteorJS"]).toEqual(
-    expect.arrayContaining(data2)
-  );
-});
+      expect.arrayContaining(data2)
+    );
+  });
 
-//Objects
-test("The first course to have a property title", () => {
+  //Objects
+  test("The first course to have a property title", () => {
     expect(data[0]).toHaveProperty("title");
-});
+  });
 
-test("The first course to have a property title and the value of 31,266", () => {
+  test("The first course to have a property title and the value of 31,266", () => {
     expect(data[0]).toHaveProperty("views", "31,266");
+  });
 });
